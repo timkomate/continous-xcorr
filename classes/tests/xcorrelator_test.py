d0a66c1141cbb08a769ccb9cdb4a35f6a52dfdd9
timko <timko.mate@gmail.com>
2019-10-04 19:32:41 +0200

    get rid of pyc files

--- classes/tests/xcorrelator_test.py
+++ classes/tests/xcorrelator_test.py
@@ -9,11 +9,13 @@ filters = [[100,10],[10,5],[5,1]]
 #filters = []
 #data = Dataset("/home/mate/PhD/codes/continous-xcorr/test_dataset",["HHZ"])
 #data = Dataset("/home/mate/PhD/codes/continous-xcorr/test_dataset",["HHZ"])#data = Dataset("/gaussdata/Seismologie/PannonianBasin/data/2017",["HHZ"])
-data = Dataset("/gaussdata/Seismologie/PannonianBasin/data",["HHZ"])
+#data = Dataset("/gaussdata/Seismologie/PannonianBasin/data",["HHZ"])
+data = Dataset("/media/timko/Maxtor/",["HHZ"])
+
 #data = Dataset("/maxwelldata/pannonian/PannonianBasin/data",["HHZ"])
 
-#data.read_dataset()
-#data.save_json("../test_py")
+data.read_dataset()
+data.save_json("../test_py")
 data.load_json("../test_py")
 intersect =  data.intersect("HHZ","Z3","A263A","HHZ","HU","BUD")
 
@@ -23,6 +25,6 @@ xc.correct_waveform_lengths()
 xc.xcorr(600)
 #xc.save_figures("./figures/")
 end = timer()
-print(end - start)  
+print(end - start)
 xc.save_ccf("./save_running")
 #xc.fft()
