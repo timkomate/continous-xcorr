bug fixes in the normalization routines

classes/tests/xcorrelator_test.py
@@ -9,24 +9,24 @@ start = timer()
 filters = [[100,10],[10,5],[5,1]]
 #filters = []
 #data = Dataset("/home/mate/PhD/codes/continous-xcorr/test_dataset",["HHZ"])
-#data = Dataset("/home/mate/PhD/codes/continous-xcorr/test_dataset",["HHZ"])
-# #data = Dataset("/gaussdata/Seismologie/PannonianBasin/data/2017",["HHZ"])
-data = Dataset("/gaussdata/Seismologie/PannonianBasin/data",["HHZ"])
+data = Dataset("/home/mate/PhD/codes/continous-xcorr/test_dataset",["HHZ"])
+#data = Dataset("/gaussdata/Seismologie/PannonianBasin/data/2017",["HHZ"])
+#data = Dataset("/gaussdata/Seismologie/PannonianBasin/data",["HHZ"])
 #data = Dataset("/media/timko/Maxtor/",["HHZ"])
 #data = Dataset("/maxwelldata/pannonian/PannonianBasin/data",["HHZ"])
 
-#data.read_dataset()
-#data.save_json("./dataset.json")
+data.read_dataset()
+data.save_json("./dataset.json")
 data.load_json("./dataset.json")
-intersect =  data.intersect("HHZ","Z3","A263A","HHZ","HU","BUD")
+intersect =  data.intersect("HHZ","Z3","A263A","HHZ","HU","ABAH")
 
-xc = Xcorrelator("HHZ","Z3","A263A","HHZ","HU","BUD", intersect, "./stations.json")
+xc = Xcorrelator("HHZ","Z3","A263A","HHZ","HU","ABAH", intersect, "./stations.json")
 xc.read_waveforms(filters= filters)
 xc.correct_waveform_lengths()
-for i in np.arange(0.1,2.5,0.1):
-    xc.xcorr(600,spectrumexp=i)
-    #xc.save_figures("./figures/")
-    end = timer()
-    print "Script finished:", end - start
-    xc.save_ccf("./ccfs", tested_parameter = "spec_exp%s" % (i))
+#for i in np.arange(0.1,2.5,0.1):
+xc.xcorr(600)
+#xc.save_figures("./figures/")
+end = timer()
+print "Script finished:", end - start
+xc.save_ccf("./ccfs")
 #xc.fft()

classes/waveform/Waveform.py
@@ -1,5 +1,6 @@
 import scipy.io
 from obspy.core.utcdatetime import UTCDateTime
+from ..xcorr_utils.xcorr_utils import downweight_ends
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal, fftpack
@@ -70,7 +71,7 @@ class Waveform(object):
         self._data[A] = 1
         self._data[np.invert(A)] = -1
 
-    def running_absolute_mean(self, filters, envsmooth = 1500, env_exp = 1.5, min_weight = 0.1):
+    def running_absolute_mean(self, filters, envsmooth = 1500, env_exp = 1.5, min_weight = 0.1, taper_length = 1000):
         self._data = signal.detrend(self._data, type="linear" )
         nb = np.floor(envsmooth/self._delta)
         weight = np.ones((self._data.shape[0]))
@@ -81,12 +82,12 @@ class Waveform(object):
         #plt.plot(self._data)
         #plt.title("unfiltered data")
         #plt.show()
-        [b,a] = signal.butter(3,[1./100/nyf, 1./1/nyf], btype='bandpass')
-        self._data = signal.filtfilt(b,a,self._data) *  signal.tukey(self._data.shape[0],alpha = 0.15)
+        #[b,a] = signal.butter(3,[1./100/nyf, 1./1/nyf], btype='bandpass')
+        #self._data = signal.filtfilt(b,a,self._data) *  signal.tukey(self._data.shape[0],alpha = 0.05)
         for filter in filters:
             #print filter
             [b,a] = signal.butter(3,[1./filter[0]/nyf, 1./filter[1]/nyf], btype='bandpass')
-            filtered_data = signal.filtfilt(b,a,self._data) *  signal.tukey(self._data.shape[0],alpha = 0.15)
+            filtered_data = downweight_ends(signal.filtfilt(b,a,self._data), wlength = taper_length * self._sampling_rate) #*  signal.tukey(self._data.shape[0],alpha = 0.01)
             #plt.plot(filtered_data)
             #plt.title("filtered data")
             #plt.show()
@@ -105,15 +106,17 @@ class Waveform(object):
         #weight = weight * data_exponent / mean_data_exponent
         water_level = np.mean(weight) * min_weight
         weight[weight < water_level] = water_level
+        nb = 2*int(taper_length*self._sampling_rate)
+        weight[:nb] = np.mean(weight)
+        weight[-nb:] = np.mean(weight)
         #plt.plot(weight)
         #plt.title("final weights")
         #plt.show()
-        self._data = (self._data / weight) *  signal.tukey(self._data.shape[0],alpha = 0.1)
+        self._data = downweight_ends((self._data / weight),wlength = taper_length * self._sampling_rate) #*  signal.tukey(self._data.shape[0],alpha = 0.1)
         #plt.plot(self._data)
         #plt.title("filtered data")
         #plt.show()
         #exit()
-        #whitened = whitened * signal.tukey(len(whitened))
 
     def print_waveform(self,extended = False):
         print "Path:", self._path

classes/xcorrealtor/Xcorrelator.py
@@ -1,12 +1,13 @@
  #import matplotlib
 #matplotlib.use('Agg')
 import glob
+from ..xcorr_utils.xcorr_utils import downweight_ends
 from ..instrument.Instrument import Instrument
 from ..station.Station import Station
 import numpy as np
 from scipy import signal, fftpack, io
 import matplotlib.pyplot as plt
-from obspy.signal.util import next_pow_2
+#from obspy.signal.util import next_pow_2
 from timeit import default_timer as timer
 import math
 import json
@@ -132,7 +133,7 @@ class Xcorrelator(object):
             b.recalculate_ntps()
             i += 1
 
-    def spectral_whitening(self, data1, spectrumexp = 0.7, espwhitening = 0.05):
+    def spectral_whitening(self, data1, spectrumexp = 0.7, espwhitening = 0.05, taper_length = 100):
         '''
         apply spectral whitening to np.array data1, divide spectrum of data1 by its smoothed version
     
@@ -144,9 +145,10 @@ class Xcorrelator(object):
             np.array, spectrally whitened time series vector
         '''
         #plt.plot(data1)
+        #plt.title("original dataset")
         #plt.show()
         spectrum =(fftpack.rfft(signal.detrend(data1,type="linear")))
-        #f = fftpack.rfftfreq(len(data1), d=1./self._sampling_rate)
+        f = fftpack.rfftfreq(len(data1), d=1./self._sampling_rate)
         spectrum_abs = np.abs(spectrum)
         water_level = np.mean(spectrum_abs)*espwhitening
         spectrum_abs[(spectrum_abs < water_level)] = water_level
@@ -154,26 +156,29 @@ class Xcorrelator(object):
         #print f, type(f), f.shape
         #plt.plot(f,spectrum)
         #plt.plot(f,spectrum_abs)
+        #plt.title("specrtum and ampl. spectrum")
         #plt.show()
         
-        #original = fftpack.irfft(spectrum)
+        original = fftpack.irfft(spectrum)
 
         #whitening
         spectrum = spectrum / (np.power(spectrum_abs,espwhitening))
-        spectrum = spectrum * signal.tukey(len(spectrum), alpha = 0.15)
+        spectrum = downweight_ends(spectrum, wlength = (taper_length * self._sampling_rate)) #* signal.tukey(len(spectrum), alpha = 0.1)
         whitened = fftpack.irfft(spectrum)
         whitened = signal.detrend(whitened,type="linear")
         whitened[0] = 0
-        whitened = whitened * signal.tukey(len(whitened), alpha = 0.15)
+        whitened =  downweight_ends(whitened,wlength= (taper_length * self._sampling_rate)) #signal.tukey(len(whitened), alpha = 0.1)
 
         nyf = (1./2)*self._sampling_rate
-        #print nyf
-        [b,a] = signal.butter(3,[(1./100)/nyf,1./1/nyf], btype='bandpass')
+        [b,a] = signal.butter(3,[(1./100)/nyf,(1./1)/nyf], btype='bandpass')
 
         #plt.plot(original)
         #plt.plot(whitened)
+        #plt.title("original and whitened before filtering")
+        #plt.show()
         whitened = signal.filtfilt(b,a,whitened)
         #plt.plot(whitened)
+        #plt.title("whitened signal after filtering")
         #plt.show()
         return whitened
         #s2 = fftpack.fft(data1)
@@ -208,7 +213,7 @@ class Xcorrelator(object):
         nstack = self._c
         station1 = self._instrument1.get_station_code()
         station2 = self._instrument2.get_station_code()
-        save_path = "%s/%s_%s_%s_%s_%s_%s_%s" % (path,corrflag,station1,station2,compflag,nstack, self._normalization_method, tested_parameter)
+        save_path = "%s/%s_%s_%s_%s_%s_%s%s" % (path,corrflag,station1,station2,compflag,nstack, self._normalization_method, tested_parameter)
         matfile = {
             "compflag" : compflag,
             "corrflag" : corrflag,